2e7c4c80ac15c9370a76f20445cb5021eb954004
chenguoguo <chenguoguochn@126.com>
2017-08-01 16:33:59 +0800

    modify arcprogress

--- app/src/main/java/com/chenguo/customviewdemo/widgets/progress/ArcProgress.java
+++ app/src/main/java/com/chenguo/customviewdemo/widgets/progress/ArcProgress.java
@@ -1,5 +1,8 @@
 package com.chenguo.customviewdemo.widgets.progress;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
@@ -8,4 +11,15 @@ import android.view.View;
  * @date: 2017/8/1 下午3:59
  */
 public class ArcProgress extends View {
+    public ArcProgress(Context context) {
+        super(context);
+    }
+
+    public ArcProgress(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ArcProgress(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
