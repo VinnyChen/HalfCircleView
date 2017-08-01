1143304a0eafb684e08071d1a428f142b14a36e6
chenguoguo <chenguoguochn@126.com>
2017-08-01 16:59:55 +0800

    create ProgressView

---
+++ app/src/main/java/com/chenguo/customviewdemo/widgets/progress/ProgressView.java
@@ -0,0 +1,25 @@
+package com.chenguo.customviewdemo.widgets.progress;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @Description:
+ * @author: chenguoguo
+ * @date: 2017/8/1 下午3:59
+ */
+public class ArcProgress extends View {
+    public ArcProgress(Context context) {
+        super(context);
+    }
+
+    public ArcProgress(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ArcProgress(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
