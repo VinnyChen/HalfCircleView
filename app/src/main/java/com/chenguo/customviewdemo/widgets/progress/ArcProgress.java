c5d364154df93cabc13181d4da3757d2f2cc41b6
chenguoguo <chenguoguochn@126.com>
2017-08-01 16:02:36 +0800

    create ArcProgress

---
+++ app/src/main/java/com/chenguo/customviewdemo/widgets/progress/ArcProgress.java
@@ -0,0 +1,11 @@
+package com.chenguo.customviewdemo.widgets.progress;
+
+import android.view.View;
+
+/**
+ * @Description:
+ * @author: chenguoguo
+ * @date: 2017/8/1 下午3:59
+ */
+public class ArcProgress extends View {
+}
